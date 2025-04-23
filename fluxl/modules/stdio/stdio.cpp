9ea4bf964b3f10cb9634fa6da0ad610fb60cfd69
oxyum services <29GariglioSe@fortcherry.org>
2025-04-23 10:17:41 -0400

    push

--- fluxl/modules/stdio/stdio.cpp
+++ fluxl/modules/stdio/stdio.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 use namespace std::string;
 
 class stdio {
     int out(string text) {
         std::cout << text << std::endl;
+        return 0;
     }
 
     int in(string text) {
-        string &text2 = text;
-    
+        string& text2 = text;
+        std::cin << text2 << std::endl;
+        return 0;
+    }
+
+    int flush() {
+        // clears output?
+        std::flush;
+        return 0;
     }
 }
