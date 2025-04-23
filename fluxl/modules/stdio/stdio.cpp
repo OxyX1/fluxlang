992c668bf7056fecf14a1bdae97d69ba55340421
oxyum services <29GariglioSe@fortcherry.org>
2025-04-23 08:17:35 -0400

    push

---
+++ fluxl/modules/stdio/stdio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+use namespace std::string;
+
+class stdio {
+    int out(string text) {
+        std::cout << text << std::endl;
+    }
+
+    int in(string text) {
+        string &text2 = text;
+    
+    }
+}
