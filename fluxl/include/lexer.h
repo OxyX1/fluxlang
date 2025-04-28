9e5d185d1cb764fd1eb5a5dadb9dbf515ba881ab
oxyum services <29GariglioSe@fortcherry.org>
2025-04-28 16:06:53 +0000

    rewrite the THOUSANTH TIME

--- fluxl/include/lexer.h
+++ fluxl/include/lexer.h
@@ -1,17 +1,30 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#include <stdbool.h>
+#include <stddef.h>  // For NULL
 
-#define MAX_LENGTH 100
+#define MAX_TOKEN_LENGTH 100
 
-// Function declarations
-bool isDelimiter(char chr);
-bool isOperator(char chr);
-bool isValidIdentifier(char* str);
-bool isKeyword(char* str);
-bool isInteger(char* str);
-char* getSubstring(char* str, int start, int end);
-void lexicalAnalyzer(char* input);
+typedef enum {
+    TOKEN_IDENTIFIER,
+    TOKEN_KEYWORD,
+    TOKEN_NUMBER,
+    TOKEN_POINTER,
+    TOKEN_INVALID,
+    TOKEN_EOF,
+    TOKEN_UNKNOWN
+} TokenType;
 
-#endif
+typedef struct {
+    TokenType type;
+    char value[MAX_TOKEN_LENGTH];
+} Token;
+
+extern const char *keywords[];
+extern const char *pointers[];
+extern const char *parenKeywords[];
+
+int is_keyword(const char *str);
+Token get_next_token(const char **src);
+
+#endif  // LEXER_H
